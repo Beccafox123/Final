6a8729bc3748f860073ecf5a92a86a925483c2bb
Beccafox123 <rebeccafox12333@gmail.com>
2016-12-14 16:11:55 -0500

    Final Assignment
    Final Assignment

---
+++ Final/Passenger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    class Passenger
+    {
+        private string Name;
+        private double Weight;
+        public static double SumOfWeight = 0;
+
+        public Passenger(string name, double weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+            SumOfWeight += weight;
+        }
+
+        public string GetName()
+        { return this.Name; }
+
+        public double GetWeight()
+        { return this.Weight; }
+
+    }
+}
